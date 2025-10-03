aa438116e139c1998af194cb5a39d2300f88d60e
Andy Kreling <krelinga@gmail.com>
2025-10-03 04:11:00 +0000

    comment out match code for now.

--- match.go
+++ match.go
@@ -1,18 +1,18 @@
 package exam
 
-import "github.com/krelinga/go-match"
+// import "github.com/krelinga/go-match"
 
-func Match[T any](e *E, got T, matcher match.Matcher[T]) bool {
-	e.Helper()
-	matched, explanation := matcher.Match(got)
-	if !matched {
-		e.Errorf("\n%s", explanation)
-	}
-	return matched
-}
+// func Match[T any](e *E, got T, matcher match.Matcher[T]) bool {
+// 	e.Helper()
+// 	matched, explanation := matcher.Match(got)
+// 	if !matched {
+// 		e.Errorf("\n%s", explanation)
+// 	}
+// 	return matched
+// }
 
-func MustMatch[T any](e *E, got T, matcher match.Matcher[T]) T {
-	e.Helper()
-	Match(e.WithFatal(), got, matcher)
-	return got
-}
+// func MustMatch[T any](e *E, got T, matcher match.Matcher[T]) T {
+// 	e.Helper()
+// 	Match(e.WithFatal(), got, matcher)
+// 	return got
+// }
--- match_test.go
+++ match_test.go
@@ -1,44 +1,44 @@
 package exam_test
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/krelinga/go-exam"
-	"github.com/krelinga/go-match"
-)
+// 	"github.com/krelinga/go-exam"
+// 	"github.com/krelinga/go-match"
+// )
 
-func TestMatch(t *testing.T) {
-	e := exam.New(t)
-	e.Run("Match Success", func(e *exam.E) {
-		got := exam.Match(e, 42, match.Equal(42))
-		if !got {
-			e.Fatal("expected match to succeed")
-		}
-	})
-	e.Run("Match Failure", func(e *exam.E) {
-		if !*manualFlag {
-			e.Skip("skipping test in manual mode")
-		}
-		got := exam.Match(e, 42, match.Equal(100))
-		if got {
-			e.Fatal("expected match to fail")
-		}
-	})
-}
+// func TestMatch(t *testing.T) {
+// 	e := exam.New(t)
+// 	e.Run("Match Success", func(e *exam.E) {
+// 		got := exam.Match(e, 42, match.Equal(42))
+// 		if !got {
+// 			e.Fatal("expected match to succeed")
+// 		}
+// 	})
+// 	e.Run("Match Failure", func(e *exam.E) {
+// 		if !*manualFlag {
+// 			e.Skip("skipping test in manual mode")
+// 		}
+// 		got := exam.Match(e, 42, match.Equal(100))
+// 		if got {
+// 			e.Fatal("expected match to fail")
+// 		}
+// 	})
+// }
 
-func TestMustMatch(t *testing.T) {
-	e := exam.New(t)
-	e.Run("MustMatch Success", func(e *exam.E) {
-		got := exam.MustMatch(e, "hello", match.Equal("hello"))
-		if got != "hello" {
-			e.Fatalf("expected got to be 'hello', got %q", got)
-		}
-	})
-	e.Run("MustMatch Failure", func(e *exam.E) {
-		if !*manualFlag {
-			e.Skip("skipping test in manual mode")
-		}
-		exam.MustMatch(e, "hello", match.NotEqual("hello"))
-		e.Fatal("expected MustMatch to fail and stop execution")
-	})
-}
+// func TestMustMatch(t *testing.T) {
+// 	e := exam.New(t)
+// 	e.Run("MustMatch Success", func(e *exam.E) {
+// 		got := exam.MustMatch(e, "hello", match.Equal("hello"))
+// 		if got != "hello" {
+// 			e.Fatalf("expected got to be 'hello', got %q", got)
+// 		}
+// 	})
+// 	e.Run("MustMatch Failure", func(e *exam.E) {
+// 		if !*manualFlag {
+// 			e.Skip("skipping test in manual mode")
+// 		}
+// 		exam.MustMatch(e, "hello", match.NotEqual("hello"))
+// 		e.Fatal("expected MustMatch to fail and stop execution")
+// 	})
+// }
