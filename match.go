a163b8e3f64d2186a9c7da295b7202d7839d5add
Andy Kreling <krelinga@gmail.com>
2025-10-03 05:24:20 +0000

    re-work Match() to use E and Recorder.

--- match.go
+++ match.go
@@ -1,18 +1,12 @@
 package exam
 
-// import "github.com/krelinga/go-match"
+import "github.com/krelinga/go-match"
 
-// func Match[T any](e *E, got T, matcher match.Matcher[T]) bool {
-// 	e.Helper()
-// 	matched, explanation := matcher.Match(got)
-// 	if !matched {
-// 		e.Errorf("\n%s", explanation)
-// 	}
-// 	return matched
-// }
-
-// func MustMatch[T any](e *E, got T, matcher match.Matcher[T]) T {
-// 	e.Helper()
-// 	Match(e.WithFatal(), got, matcher)
-// 	return got
-// }
+func Match[T any](e E, got T, matcher match.Matcher[T]) *Result {
+	e.Helper()
+	matched, explanation := matcher.Match(got)
+	if !matched {
+		e.Errorf("\n%s", explanation)
+	}
+	return NewResult(e, matched)
+}
